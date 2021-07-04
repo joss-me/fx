d7d80884e7bf27e5130f9a8cd44e2cc4f6f21f59
joss-me <54775248+joss-me@users.noreply.github.com>
2021-07-04 23:20:16 +0700

    Create stream.php

---
+++ stream.php
@@ -0,0 +1,14 @@
+<?php
+ include 'src/VideoStreamer.php'; 
+set_time_limit(0);
+
+$url = isset($_GET['url']) ? $_GET['url'] : null;
+
+if ($url == false) {
+    die("No url provided");
+}
+
+$video = new \Video\VideoStreamer();
+$video->stream($url);
+
+?>
